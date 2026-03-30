8b7fd81742e2b6c00f734986d9478935827dc2b0
Gui-SESI09 <guilherme_machado18@estudante.sesisenai.org.br>
2026-03-30 19:48:09 -0300

    Novos métodos criados

--- server.js
+++ server.js
@@ -10,31 +10,40 @@ const sql = new Pool({
 })
 
 const servidor = Fastify();
-//Preparando novo método
-servidor.metodo('/a', async (request, reply) => {
-    // 1. Pegar os dados que o usuário enviou (body ou params)
-    const body = request.body;
-    if(!body || !body. || !body.){
-        return reply.status().send({ message: ""})
-    }
-    // 2. Executar o comando SQL usando os dados acima
-    await sql.query(', [body., body., body.')
-    // 3. Devolver uma resposta (sucesso ou erro)
-    reply.status().send({message : ""})
-})
 
 //POST RF01 E RF08: Criar chamado com responsável E conter título e descrição.
 servidor.post('/chamados', async (request, reply) => {
     
     const body = request.body;
     if(!body || !body.titulo || !body.descricao || !body.usuario_id){
-        return reply.status(400).send({ message: "Dados não encontrados verifique o título, a descrição ou o id do usuário!"})
+        return reply.status(400).send({ message: "Dados não encontrados verifique o título, a descrição ou o id do usuário!"});
     }
     
-    await sql.query('INSERT INTO chamados (titulo, descricao, usuario_id) VALUES ($1, $2, $3)', [body.titulo, body.descricao, body.usuario_id]) 
+    await sql.query('INSERT INTO chamados (titulo, descricao, usuario_id) VALUES ($1, $2, $3)', [body.titulo, body.descricao, body.usuario_id]);
     
-    reply.status(201).send({ message: 'Chamado foi aberto com sucesso!'})
+    reply.status(201).send({ message: 'Chamado foi aberto com sucesso!'});
 })
+
+//POST RF02 E RF03: Chamados através de um filtro por responsável.
+servidor.get('/chamados/:usuario_id', async (request, reply) => {
+    const body = request.body;
+    if(!body || !body.id){
+        return reply.status(400).send({ message: "ID de usuário não existe ou foi inserido incorretamente!"});
+    }
+    await sql.query('SELECT * FROM chamados WHERE usuario_id = $1,' [body.usuario_id]);
+    
+    reply.status(201).send({message : "Tudo está correto, veja os chamados referentes ao usuário solicitado!"});
+});
+
+//POST RF05: Deletar Chamados
+servidor.delete('/chamados/:id', async (request, reply) => {
+    const body = request.body;
+    if(!body || !body.id){
+        return reply.status(400).send({ message: "Não foi possível deletar esse chamado, tente novamente!"});
+    }
+    await sql.query('DELETE FROM chamados WHERE id = $1', [body.id]);
+    reply.status(201).send({message : "Tudo está correto, seu chamado foi deletado!"});
+});
 
 servidor.listen({
     port: 3000
